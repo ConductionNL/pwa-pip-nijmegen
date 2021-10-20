13e6cf76a92d69d216032687df5463037ee72560
Barry Brands <barrybrands02@hotmail.com>
2021-10-20 11:40:22 +0200

    Home page

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -10,25 +10,61 @@ import {useRouter} from "next/router";
 import PaperCard from "../components/common/paperCard";
 import {useUserContext} from "../components/context/userContext";
 import {useAppContext} from "../components/context/state";
-import { Typography } from '@material-ui/core';
+import { Avatar, Box, IconButton, Typography, Link } from '@material-ui/core';
 import Button from "@material-ui/core/Button";
 
-const Welcome = () => (
-  <>
+
+
+export default function Index() {
+  let context = useAppContext();
+
+  const noHoverStyle = {
+    '&:hover': {
+      background: 'none',
+    }
+  }
+
+
+  return (<>
+
     <Layout title="Welkom bij Mijn Omgeving Nijmegen" description="waar kan ik deze description zien">
-      <Grid container spacing={2}>
+      <Grid sx={{paddingTop: "20px"}} container spacing={2}>
         <Grid item xs={12}>
           <Typography variant="h3" component="h3">
-            Welkom op Mijn Omgeving!
-            <Button>
-              Test
-              </Button>
+            Mijn Nijmegen
           </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h6">
+            Bekijk uw gegevens, meldingen, aanvragen of uitkeringsgegevens op uw persoonlijke gemeentepagina.
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Box style={{ padding: '15px', background: '#157C68', width: '265px', marginTop: '20px' }}>
+            <Link href={context.baseUrl + " /digid/login?returnUrl=" + context.frontendUrl + "/moving?state=8412312632"} style={{ textDecoration: 'none !important' }}>
+               <img src="/digid_button.svg" width='55px' height='55px'  />
+               <b style={{ textAlign: 'center', color: 'white',  verticalAlign: 'middle', paddingLeft: '45px'}}>
+                 INLOGGEN
+              </b>
+            </Link>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography style={{color: '#A80A2D', marginTop: '20px'}} variant="h4" component="h4">
+            Wat moet u weten 
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <p>In Mijn Nijmegen vindt u: </p>
+          <ul>
+            <li>Uw persoonsgegevens</li>
+            <li>Gegevens over uw diensten</li>
+            <li>Gegevens over uw zaken</li>
+            <li>Uw informatie in uw kluis</li>
+          </ul>
         </Grid>
       </Grid>
     </Layout>
-  </>
-);
-
-export default Welcome;
+  </>);
+}
 
