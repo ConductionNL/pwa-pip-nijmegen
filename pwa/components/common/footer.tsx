150b36d04418da9d5c47fad7e0376e064dfa1368
Barry Brands <barrybrands02@hotmail.com>
2021-10-20 13:56:52 +0200

    Footer improvements

--- pwa/components/common/footer.tsx
+++ pwa/components/common/footer.tsx
@@ -20,7 +20,10 @@ import LogoutIcon from "@mui/icons-material/ExitToApp";
 import {useUserContext} from "../context/userContext";
 import Typography from "@mui/material/Typography";
 import {Login} from "@mui/icons-material";
-import {useAppContext} from "../context/state";
+import { useAppContext } from "../context/state";
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const useStyles = makeStyles((theme) => ({
   footerStyle: {
@@ -46,6 +49,13 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     },
   },
+  footerItem: {
+    textDecoration: "none !important",
+    '&:hover': {
+      textDecoration: "underline !important",
+      color: "white"
+    },
+  }
 }));
 
 export default function Footer() {
@@ -82,21 +92,21 @@ export default function Footer() {
 
   return (
     <footer>
-      <Paper style={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation
-          value={state['bottumNavigation']} onChange={handleChange}
-          showLabels
-          className={classes.sectionMobile}
-        >
+      {/*<Paper style={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>*/}
+      {/*  <BottomNavigation*/}
+      {/*    value={state['bottumNavigation']} onChange={handleChange}*/}
+      {/*    showLabels*/}
+      {/*    className={classes.sectionMobile}*/}
+      {/*  >*/}
 
-          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction label="Klantvragen" icon={<SearchIcon />} />
-          <BottomNavigationAction label="Notificaties" icon={<NotificationsIcon />} />
-          <BottomNavigationAction label="Chatten" icon={<ChatIcon />} />
-          <BottomNavigationAction label="Logout" icon={<LogoutIcon />} />
-        </BottomNavigation>
-      </Paper>
-      <Box
+      {/*    <BottomNavigationAction label="Home" icon={<HomeIcon />} />*/}
+      {/*    <BottomNavigationAction label="Klantvragen" icon={<SearchIcon />} />*/}
+      {/*    <BottomNavigationAction label="Notificaties" icon={<NotificationsIcon />} />*/}
+      {/*    <BottomNavigationAction label="Chatten" icon={<ChatIcon />} />*/}
+      {/*    <BottomNavigationAction label="Logout" icon={<LogoutIcon />} />*/}
+      {/*  </BottomNavigation>*/}
+      {/*</Paper>*/}
+      <Box 
         px={{ xs: 3, sm: 10 }}
         py={{ xs: 5, sm: 10 }}
         bgcolor="#3A3A3A"
@@ -105,40 +115,53 @@ export default function Footer() {
       >
         <Container maxWidth="lg">
           <Grid container spacing={5}>
-            <Grid item xs={12} sm={6}>
-              <Box borderBottom={1}>Help</Box>
+            <Grid item xs={12} sm={4}>
               <Box>
-                <Link href="/" color="inherit">
-                  Contact
-                </Link>
+                <Typography variant="h5" component="h5" style={{ textDecoration: 'none' }}>
+                  Diensten
+                </Typography>
               </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Support
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Privacy
+              <Box sx={{marginBottom: '15px', marginTop: '15px'}}>
+                <Link href="/" color="inherit" className={classes.footerItem}>
+                  Verhuizen
                 </Link>
               </Box>
             </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <Box borderBottom={1}>Contact</Box>
-              <Box>Stadswinkel, Mariënburg 30
-              </Box>
+            <Grid item xs={12} sm={4}>
               <Box>
-                <Link href="tel:14 024" color="inherit">
-                  14 024
+                <Typography variant="h5" component="h5" style={{ textDecoration: 'none '}}>
+                  Contact
+                </Typography>
+              </Box>
+              <Box sx={{ marginBottom: '15px', marginTop: '15px' }}>
+                <LocationOnIcon/> Stadswinkel, Mariënburg 30
+              </Box>
+              <Box sx={{ marginBottom: '15px', marginTop: '15px' }}>
+                <Link href="tel:14 024" color="inherit" className={classes.footerItem}>
+                  <CallIcon/> 14 024
                 </Link>
               </Box>
-              <Box>
-                <Link href="mailto:gemeente@nijmegen.nl" color="inherit">
-                  gemeente@nijmegen.nl
+              <Box sx={{ marginBottom: '15px', marginTop: '15px' }}>
+                <Link href="mailto:gemeente@nijmegen.nl" color="inherit" className={classes.footerItem}>
+                  <EmailIcon/> gemeente@nijmegen.nl
                 </Link>
               </Box>
             </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Box>
+                <Typography variant="h5" component="h5" style={{ textDecoration: 'none'}}>
+                  Openingstijden
+                </Typography>
+              </Box>
+              <Box sx={{ marginBottom: '15px', marginTop: '15px' }}>
+                Maandag - woensdag: 9.00 - 17.00 <br/>
+                Donderdag: 9.00 - 20.00 <br/>
+                Vrijdag: 9.00 - 17.00
+              </Box>
+            </Grid>
+
           </Grid>
         </Container>
       </Box>
