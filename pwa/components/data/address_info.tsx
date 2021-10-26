f4f71b802e461c7cef079c11e0e373abe734e893
Sarai Misidjan <sarai@conduction.nl>
2021-10-26 10:51:37 +0200

    fix mijn gegevens page

--- pwa/components/data/address_info.tsx
+++ pwa/components/data/address_info.tsx
@@ -1,24 +1,96 @@
-import React from 'react'
-import Box from "@material-ui/core/Box";
+import React, {ReactNode} from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import {Divider, Typography} from "@mui/material";
+import {styled} from '@mui/material/styles';
+import MuiGrid from '@mui/material/Grid';
+import {makeStyles} from "@material-ui/core/styles";
 import {useGet} from "restful-react";
 
+const useStyles = makeStyles({
+  labelData: {
+    color: "gray",
+  },
+  data: {
+    marginLeft: "-60%"
+  },
+});
 
-export default function AddressInfo({person = null, id = null}) {
-  if (person == null) {
-    var { data: person } = useGet({
-      path: "gateways/brp/ingeschrevenpersonen/" + id,
+export default function AddressList({data = null}) {
+  const classes = useStyles();
+  const title = 'Mijn gegevens';
+
+  const StyledGrid = styled(MuiGrid)(({theme}) => ({
+    width: '100%',
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+      // margin: theme.spacing(0, 0),
+      float: "left",
+      textAlign: "left"
+    },
+  }));
+
+  if (data == null) {
+    var {data: data} = useGet({
+      path: "/data"
     });
   }
-  // console.log(person)
+
+  /* lets catch hydra */
+  if (data != null && data["results"] !== undefined) {
+    data = data["results"];
+
+    for (let i = 0; i < data.length; i++) {
+      data[i].id = data[i].identificatie;
+    }
+  }
+
+  const [open, setOpen] = React.useState(true);
+  const [openAlert, setOpenAlert] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const handleAlertClick = () => {
+    setOpenAlert(!openAlert);
+  };
 
   return (
-    <Box paddingTop={1}>
-      {
-        person != null && person.verblijfplaats !== null &&
-        <p>
-          <b>Verblijfplaats: </b>{person.verblijfplaats.adresregel1 + ", " + person.verblijfplaats.adresregel2 + ", " + person.verblijfplaats.adresregel3}
-        </p>
-      }
+    <Box paddingTop={2} paddingBottom={2}>
+      <Grid container paddingTop={3}>
+        <StyledGrid paddingLeft={6} item xs/>
+        <StyledGrid item xs className={classes.data}>
+          <Typography variant="h5" style={{fontWeight: 'bold'}}>Woonadres</Typography>
+        </StyledGrid>
+      </Grid>
+
+      <Grid container paddingTop={3}>
+        <StyledGrid paddingLeft={6} item xs>
+          <p className={classes.labelData}>Straat</p>
+        </StyledGrid>
+        <StyledGrid item xs className={classes.data}>
+          <p>Vuntuslaan 75</p>
+        </StyledGrid>
+      </Grid>
+
+      <Grid container>
+        <StyledGrid paddingLeft={6} item xs>
+          <p className={classes.labelData}>Plaats</p>
+        </StyledGrid>
+        <StyledGrid item xs className={classes.data}>
+          <p>1231 NR Loosdrecht</p>
+        </StyledGrid>
+      </Grid>
+
+      <Grid container>
+        <StyledGrid paddingLeft={6} item xs>
+          <p className={classes.labelData}>Vanaf</p>
+        </StyledGrid>
+        <StyledGrid item xs className={classes.data}>
+          <p>01 januari 2002</p>
+        </StyledGrid>
+      </Grid>
     </Box>
   );
 }
--- pwa/components/data/personal_info.tsx
+++ pwa/components/data/personal_info.tsx
@@ -1,34 +1,96 @@
-import React from 'react'
-import Box from "@material-ui/core/Box";
-import {useGet, Poll, Get} from "restful-react";
-import AddressInfo from "../../components/data/address_info";
+import React, {ReactNode} from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import {Divider, Typography} from "@mui/material";
+import {styled} from '@mui/material/styles';
+import MuiGrid from '@mui/material/Grid';
+import {makeStyles} from "@material-ui/core/styles";
+import {useGet} from "restful-react";
 
+const useStyles = makeStyles({
+  labelData: {
+    color: "gray",
+  },
+  data: {
+    marginLeft: "-60%"
+  },
+});
 
-export default function PersonalInfo({id = null, person = null}) {
-  if (person == null) {
-    var { data: person } = useGet({
-      path: "gateways/brp/ingeschrevenpersonen/" + id,
+export default function PersonalList({data = null}) {
+  const classes = useStyles();
+  const title = 'Mijn gegevens';
+
+  const StyledGrid = styled(MuiGrid)(({theme}) => ({
+    width: '100%',
+    ...theme.typography.body2,
+    '& [role="separator"]': {
+      // margin: theme.spacing(0, 0),
+      float: "left",
+      textAlign: "left"
+    },
+  }));
+
+  if (data == null) {
+    var {data: data} = useGet({
+      path: "/data"
     });
   }
-  // console.log(person)
+
+  /* lets catch hydra */
+  if (data != null && data["results"] !== undefined) {
+    data = data["results"];
+
+    for (let i = 0; i < data.length; i++) {
+      data[i].id = data[i].identificatie;
+    }
+  }
+
+  const [open, setOpen] = React.useState(true);
+  const [openAlert, setOpenAlert] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const handleAlertClick = () => {
+    setOpenAlert(!openAlert);
+  };
 
   return (
-    <Box paddingBottom={2}>
-      <h4>Uw persoonlijke informatie</h4>
-      <Box paddingTop={1}>
-        {
-          person != null && person.naam.aanschrijfwijze !== null &&
-          <p><b>Naam: </b>{person.naam.aanschrijfwijze}</p>
-        }
-        {
-          person != null && person.geboorte.plaats.omschrijving !== null &&
-          <p><b>Geboorteplaats: </b>{person.geboorte.plaats.omschrijving}</p>
-        }
-        {
-          person != null && person.verblijfplaats !== null &&
-          <AddressInfo person={person}/>
-        }
-      </Box>
+    <Box paddingTop={2} paddingBottom={2}>
+      <Grid container paddingTop={3}>
+        <StyledGrid paddingLeft={6} item xs/>
+        <StyledGrid item xs className={classes.data}>
+          <Typography variant="h5" style={{fontWeight: 'bold'}}>Persoonlijke gegevens</Typography>
+        </StyledGrid>
+      </Grid>
+
+      <Grid container paddingTop={3}>
+        <StyledGrid paddingLeft={6} item xs>
+          <p className={classes.labelData}>Voornamen</p>
+        </StyledGrid>
+        <StyledGrid item xs className={classes.data}>
+          <p>Sarai Aliza</p>
+        </StyledGrid>
+      </Grid>
+
+      <Grid container>
+        <StyledGrid paddingLeft={6} item xs>
+          <p className={classes.labelData}>Achternaam</p>
+        </StyledGrid>
+        <StyledGrid item xs className={classes.data}>
+          <p>Misidjan</p>
+        </StyledGrid>
+      </Grid>
+
+      <Grid container>
+        <StyledGrid paddingLeft={6} item xs>
+          <p className={classes.labelData}>Geslacht</p>
+        </StyledGrid>
+        <StyledGrid item xs className={classes.data}>
+          <p>Vrouw</p>
+        </StyledGrid>
+      </Grid>
     </Box>
   );
 }
