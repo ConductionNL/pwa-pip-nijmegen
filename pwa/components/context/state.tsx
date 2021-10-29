9b79be73e7daeb7593f58892d1d78589c0802a18
ginokok1996 <ginokok1996@hotmail.com>
2021-10-29 13:13:52 +0200

    local dev

--- pwa/components/context/state.tsx
+++ pwa/components/context/state.tsx
@@ -5,10 +5,10 @@ const AppContext = createContext(undefined);
 export function AppWrapper({ children }) {
 
   let sharedState = {
-    meUrl: 'https://nijmegen.commonground.nu/api/users/me',
-    apiUrl: 'https://nijmegen.commonground.nu/api',
-    baseUrl: 'https://nijmegen.commonground.nu',
-    frontendUrl: 'https://nijmegen.commonground.nu',
+    meUrl: 'http://localhost/me',
+    apiUrl: 'http://localhost/api',
+    baseUrl: 'http://localhost',
+    frontendUrl: 'http://localhost:3000',
     organization: 'http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43',
     brpUrl: 'http://brpservice.waardepapieren.svc.cluster.local',
   }
