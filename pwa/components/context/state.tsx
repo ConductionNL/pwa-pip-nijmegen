f263ffd6f7989f53700bb83cc18df76539e6a618
ginokok1996 <ginokok1996@hotmail.com>
2021-10-29 11:07:07 +0200

    context url's update

--- pwa/components/context/state.tsx
+++ pwa/components/context/state.tsx
@@ -4,14 +4,41 @@ const AppContext = createContext(undefined);
 
 export function AppWrapper({ children }) {
 
-  let sharedState = {
-    meUrl: process.env.NEXT_PUBLIC_ME_URL !== undefined ? process.env.NEXT_PUBLIC_ME_URL : 'http://localhost/me',
-    apiUrl: process.env.NEXT_PUBLIC_API_URL !== undefined ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost/api',
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL !== undefined ? process.env.NEXT_PUBLIC_BASE_URL : 'http://localhost',
-    frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_URL !== undefined ? process.env.NEXT_PUBLIC_FRONTEND_URL : 'http://localhost:3000',
-    organization: process.env.NEXT_PUBLIC_ORGANIZATION !== undefined ? process.env.NEXT_PUBLIC_ORGANIZATION : 'http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43',
-    brpUrl: process.env.NEXT_PUBLIC_BRP_URL !== undefined ? process.env.NEXT_PUBLIC_BRP_URL : 'http://brpservice.waardepapieren.svc.cluster.local',
+  const crypto = require('crypto');
+  let nonce = crypto.randomBytes(16).toString('base64');
+
+  let sharedState = {};
+  let meUrl;
+  let apiUrl;
+  let baseUrl;
+  let frontendUrl;
+  let organization;
+
+  if (typeof window !== 'undefined') {
+    if (window.location.href.includes('http://localhost')) {
+      meUrl = 'http://localhost/me';
+      apiUrl = 'http://localhost/api';
+      baseUrl = 'http://localhost';
+      frontendUrl = 'http://localhost:3000';
+      organization = 'http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43';
+    } else {
+      meUrl = 'https://nijmegen.commonground.nu/api/users/me';
+      apiUrl = 'https://nijmegen.commonground.nu//api';
+      baseUrl = 'https://nijmegen.commonground.nu/';
+      frontendUrl = 'https://nijmegen.commonground.nu/';
+      organization = 'http://webresourcecatalogus.verhuizen.svc.cluster.local/organizations/4f387d0e-a2e5-44c0-9902-c31b63a8ee36';
+    }
   }
+
+  sharedState = {
+    meUrl: meUrl,
+    apiUrl: apiUrl,
+    baseUrl: baseUrl,
+    frontendUrl: frontendUrl,
+    organization: organization,
+    nonce: nonce
+  }
+
 
   return (
     <AppContext.Provider value={sharedState}>
