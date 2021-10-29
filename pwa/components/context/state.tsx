8789fa2aaf27ed75ce6cc2b4ee2a7a64decd0aca
ginokok1996 <ginokok1996@hotmail.com>
2021-10-29 11:47:56 +0200

    hard fix for online deploy

--- pwa/components/context/state.tsx
+++ pwa/components/context/state.tsx
@@ -4,37 +4,14 @@ const AppContext = createContext(undefined);
 
 export function AppWrapper({ children }) {
 
-  let sharedState = {};
-  let meUrl;
-  let apiUrl;
-  let baseUrl;
-  let frontendUrl;
-  let organization;
-
-  if (typeof window !== 'undefined') {
-    if (window.location.href.includes('http://localhost')) {
-      meUrl = 'http://localhost/me';
-      apiUrl = 'http://localhost/api';
-      baseUrl = 'http://localhost';
-      frontendUrl = 'http://localhost:3000';
-      organization = 'http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43';
-    } else {
-      meUrl = 'https://nijmegen.commonground.nu/api/users/me';
-      apiUrl = 'https://nijmegen.commonground.nu/api';
-      baseUrl = 'https://nijmegen.commonground.nu/';
-      frontendUrl = 'https://nijmegen.commonground.nu/';
-      organization = 'http://webresourcecatalogus.verhuizen.svc.cluster.local/organizations/4f387d0e-a2e5-44c0-9902-c31b63a8ee36';
-    }
+  let sharedState = {
+    meUrl: 'https://nijmegen.commonground.nu/api/user/me',
+    apiUrl: 'https://nijmegen.commonground.nu/api',
+    baseUrl: 'https://nijmegen.commonground.nu',
+    frontendUrl: 'https://nijmegen.commonground.nu',
+    organization: 'http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43',
+    brpUrl: 'http://brpservice.waardepapieren.svc.cluster.local',
   }
-
-  sharedState = {
-    meUrl: meUrl,
-    apiUrl: apiUrl,
-    baseUrl: baseUrl,
-    frontendUrl: frontendUrl,
-    organization: organization,
-  }
-
 
   return (
     <AppContext.Provider value={sharedState}>
